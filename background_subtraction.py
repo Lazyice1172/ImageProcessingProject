6e045441a8eef3d6e031f44e250d5fc4c10dd71c
C20372876 Jaycel Estrellado <C20372876@mydit.ie>
2023-11-27 19:45:43 +0000

    Added videos files + some changes

--- background_subtraction.py
+++ background_subtraction.py
@@ -3,8 +3,9 @@ import cv2 as cv
 
 # Function
 def filter_img(frame):
+    # gets Morph Ellipse shapes -> elliptic shape 
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
-    _, thresh = cv.threshold(frame, 244, 255, cv.THRESH_BINARY)
+    _, thresh = cv.threshold(frame, 250, 255, cv.THRESH_BINARY)
     erode = cv.erode(thresh, kernel, iterations=2)
     dilated = cv.dilate(erode, kernel, iterations=2)
 
@@ -21,32 +22,37 @@ def filter_img(frame):
 # Better shadow detect and accuracy
 object_detector = cv.createBackgroundSubtractorKNN(detectShadows=True)
 
-cap = cv.VideoCapture("chosen_video.mp4")
+cap = cv.VideoCapture("videos/Snowy-Area.mp4")
 
 while True:
 
     ret, frame = cap.read()
-    ret, frame1 = cap.read()
 
     #  Object detector proccesses the video to identify objects, in this case - vehicles
-    obj_detector = object_detector.apply(frame, frame1)
+    obj_detector = object_detector.apply(frame)
 
     mask = filter_img(obj_detector)
 
     contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    
+    frameCopy = frame.copy()
+
     for contour in contours:
-        (x, y, w, h) = cv.boundingRect(contour)
-        if cv.contourArea(contour) < 400:
+
+        # 100-200: notices trees but detects motorcycles. 300-400: no trees, doesnt detect motorcycles.
+        if cv.contourArea(contour) < 100:
             continue
-        cv.rectangle(frame, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
-        cv.putText(frame, 'Vehicle Detected', (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.3, (0,255,0), 1, cv.LINE_AA)
+        (x, y, w, h) = cv.boundingRect(contour)
+
+        cv.rectangle(frameCopy, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
+        cv.putText(frameCopy, 'Vehicle Detected', (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.3, (0,255,0), 1, cv.LINE_AA)
 
     # cv.drawContours(frame1, contours, -1, (0, 255, 0), 3)
 
-    #foregroundPart = cv.bitwise_and(frame,frame, mask=mask)
+    foregroundPart = cv.bitwise_and(frame,frame, mask=mask)
 
-    cv.imshow("Frame", cv.resize(frame, None, fx=0.4, fy=0.4))
-    cv.imshow("Mask", cv.resize(mask, None, fx=0.4, fy=0.4))
+    cv.imshow("Frame", cv.resize(frameCopy, None, fx=0.4, fy=0.4))
+    cv.imshow("Mask", cv.resize(foregroundPart, None, fx=0.4, fy=0.4))
 
     if cv.waitKey(50) == ord("q"):
         break
--- frame_difference.py
+++ frame_difference.py
@@ -14,7 +14,7 @@ def filter_img(frame):
 
 
 # Main
-cap = cv.VideoCapture("chosen_video.mp4")
+cap = cv.VideoCapture("videos/chosen_video.mp4")
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
@@ -25,17 +25,22 @@ while cap.isOpened():
 
     mask = filter_img(diff)
 
-    contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    contours,_= cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
     for contour in contours:
+        
         (x, y, w, h) = cv.boundingRect(contour)
+        
         if cv.contourArea(contour) < 500:
             continue
+
         cv.rectangle(frame1, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
+
 
         cv.putText(frame1, 'Vehicle Detected', (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.3, (0,255,0), 1, cv.LINE_AA)
 
     foregroundPart = cv.bitwise_and(frame1,frame1, mask=mask)
-    # cv.drawContours(frame1, contours, -1, (0, 255, 0), 3)
+    
 
     cv.imshow("Frame", cv.resize(frame1, None, fx=0.4, fy=0.4))
     cv.imshow("Mask", cv.resize(foregroundPart, None, fx=0.4, fy=0.4))
