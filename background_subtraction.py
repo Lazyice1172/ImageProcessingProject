744002813645a1ca9a0a728c3cb0a1e992fd88ea
C20372876 Jaycel Estrellado <C20372876@mydit.ie>
2023-11-27 17:17:17 +0000

    Added 2nd frame reading for better functionality

--- background_subtraction.py
+++ background_subtraction.py
@@ -5,8 +5,8 @@ import cv2 as cv
 def filter_img(frame):
     kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE, (3, 3))
     _, thresh = cv.threshold(frame, 244, 255, cv.THRESH_BINARY)
-    thresh = cv.erode(thresh, kernel, iterations=2)
-    dilated = cv.dilate(thresh, kernel, iterations=2)
+    erode = cv.erode(thresh, kernel, iterations=2)
+    dilated = cv.dilate(erode, kernel, iterations=2)
 
     return dilated
 
@@ -24,22 +24,26 @@ object_detector = cv.createBackgroundSubtractorKNN(detectShadows=True)
 cap = cv.VideoCapture("chosen_video.mp4")
 
 while True:
+
     ret, frame = cap.read()
+    ret, frame1 = cap.read()
 
- 
-    mask = object_detector.apply(frame)
+    #  Object detector proccesses the video to identify objects, in this case - vehicles
+    obj_detector = object_detector.apply(frame, frame1)
 
-    mask = filter_img(mask)
+    mask = filter_img(obj_detector)
 
     contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         (x, y, w, h) = cv.boundingRect(contour)
-        if cv.contourArea(contour) < 100:
+        if cv.contourArea(contour) < 400:
             continue
         cv.rectangle(frame, pt1=(x, y), pt2=(x + w, y + h), color=(0, 255, 0), thickness=2)
         cv.putText(frame, 'Vehicle Detected', (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 0.3, (0,255,0), 1, cv.LINE_AA)
 
     # cv.drawContours(frame1, contours, -1, (0, 255, 0), 3)
+
+    #foregroundPart = cv.bitwise_and(frame,frame, mask=mask)
 
     cv.imshow("Frame", cv.resize(frame, None, fx=0.4, fy=0.4))
     cv.imshow("Mask", cv.resize(mask, None, fx=0.4, fy=0.4))
